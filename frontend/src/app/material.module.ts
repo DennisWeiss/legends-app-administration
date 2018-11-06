3b7afec8bce5e5cbaa0c0ad57ea841de60c8f9b9
Sebastian Schmidt <schmidtseb@outlook.de>
2018-11-06 20:28:31 +0100

    Configured app to work with Angular Material. MatComponents should be added in app/material.module.ts

---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { MatCheckboxModule } from "@angular/material";
+
+
+@NgModule({
+  exports: [
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule {}
