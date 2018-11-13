dc490bc707d8240e233b2870a31eaa1eb8b023dc
Dennis Weiss <dennis.weiss@stud.hszg.de>
2018-11-13 09:31:15 +0100

    Introducing responsive design for poi edit page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,32 +1,34 @@
 import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { PoiOverviewComponent } from './poi-overview/poi-overview.component'
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {PoiOverviewComponent} from './poi-overview/poi-overview.component'
 import {HttpClientModule} from '@angular/common/http';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { HeaderComponent } from './header/header.component'
+import {SignupComponent} from './auth/signup/signup.component';
+import {LoginComponent} from './auth/login/login.component';
+import {SidenavComponent} from './sidenav/sidenav.component';
+import {HeaderComponent} from './header/header.component'
 import {FlexLayoutModule} from '@angular/flex-layout';
-import { TagComponent } from './tag/tag.component';
+import {TagComponent} from './tag/tag.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import { PoiFilterComponent } from './poi-filter/poi-filter.component';
-import { PoiTypeSelectionComponent } from './poi-type-selection/poi-type-selection.component';
-import { PoiEditComponent } from './poi-edit/poi-edit.component';
-import { PoiMapComponent } from './poi-edit/poi-map/poi-map.component';
-import { ImgPreviewComponent } from './poi-edit/img-preview/img-preview.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { ImgPreviewMultipleComponent } from './poi-edit/img-preview-multiple/img-preview-multiple.component';
-import { VideoPreviewComponent } from './poi-edit/video-preview/video-preview.component';
-import { HtmlEditorComponent } from './poi-edit/html-editor/html-editor.component';
-import { QuillModule } from 'ngx-quill';
-import { PoiContentComponent } from './poi-edit/poi-content/poi-content.component'
-import { ReactiveFormsModule } from '@angular/forms';
-import { MediaSectionComponent } from './poi-edit/media-section/media-section.component';
+import {PoiFilterComponent} from './poi-filter/poi-filter.component';
+import {PoiTypeSelectionComponent} from './poi-type-selection/poi-type-selection.component';
+import {PoiEditComponent} from './poi-edit/poi-edit.component';
+import {PoiMapComponent} from './poi-edit/poi-map/poi-map.component';
+import {ImgPreviewComponent} from './poi-edit/img-preview/img-preview.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {ImgPreviewMultipleComponent} from './poi-edit/img-preview-multiple/img-preview-multiple.component';
+import {VideoPreviewComponent} from './poi-edit/video-preview/video-preview.component';
+import {HtmlEditorComponent} from './poi-edit/html-editor/html-editor.component';
+import {QuillModule} from 'ngx-quill';
+import {PoiContentComponent} from './poi-edit/poi-content/poi-content.component'
+import {ReactiveFormsModule} from '@angular/forms';
+import {MediaSectionComponent} from './poi-edit/media-section/media-section.component';
+import {NgxUploaderModule} from "ngx-uploader";
+
 
 @NgModule({
   declarations: [
@@ -58,11 +60,10 @@ import { MediaSectionComponent } from './poi-edit/media-section/media-section.co
     FontAwesomeModule,
     LeafletModule.forRoot(),
     QuillModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxUploaderModule
   ],
-  exports: [
-
-  ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/poi-edit/img-preview/img-preview.component.ts
+++ frontend/src/app/poi-edit/img-preview/img-preview.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, EventEmitter} from '@angular/core';
 import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
+import {UploaderOptions, UploadFile, UploadInput, UploadOutput} from "ngx-uploader";
 
 @Component({
   selector: 'app-img-preview',
@@ -10,10 +11,46 @@ export class ImgPreviewComponent implements OnInit {
 
   @Input() name: string;
 
-  imgLoaded = true;
-  faPlusCircle = faPlusCircle;
+  imgLoaded = false;
 
-  constructor() { }
+  options: UploaderOptions;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  dragOver;
+
+  constructor() {
+    this.options = { concurrency: 1, maxUploads: 3 };
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+  }
+
+  onUploadOutput(output: UploadOutput): void {
+    if (output.type === 'allAddedToQueue') { // when all files added in queue
+      const event: UploadInput = {
+        type: 'uploadAll',
+        url: '/upload',
+        method: 'POST',
+        data: { foo: 'bar' }
+      };
+      this.uploadInput.emit(event);
+      this.imgLoaded = true
+    } else if (output.type === 'addedToQueue'  && typeof output.file !== 'undefined') { // add file to array when added
+      this.files.push(output.file);
+    } else if (output.type === 'uploading' && typeof output.file !== 'undefined') {
+      // update current data in files array for uploading file
+      const index = this.files.findIndex(file => typeof output.file !== 'undefined' && file.id === output.file.id);
+      this.files[index] = output.file;
+    } else if (output.type === 'removed') {
+      // remove file from array when removed
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    } else if (output.type === 'dragOver') {
+      this.dragOver = true;
+    } else if (output.type === 'dragOut') {
+      this.dragOver = false;
+    } else if (output.type === 'drop') {
+      this.dragOver = false;
+    }
+  }
 
   ngOnInit() {
   }
