736b82ea8856581b533123b2d9d74cd4dec13da2
Sebastian Schmidt <schmidtseb@outlook.de>
2018-11-13 03:06:05 +0100

    Extended poi-service.

--- frontend/src/app/poi.service.ts
+++ frontend/src/app/poi.service.ts
@@ -1,8 +1,13 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { getPreviousOrParentTNode } from '@angular/core/src/render3/instructions';
 
 
 const backendUrl = 'http://localhost:3000/'
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +18,21 @@ export class PoiService {
   }
 
   retrievePOIs = (type?: String) => this.http.get(`${backendUrl}poi/${type ? `${type}/` : ''}`)
+
+
+  /**
+   * TODO: Create model for POI
+   */
+
+  getPOI (id, type): Observable<any> {
+    return this.http.get<any>(`${backendUrl}poi/${type}/${id}`);
+  }
+
+  postFile(fileToUpload: File): Observable<boolean> {
+    const endpoint = backendUrl;
+    const formData: FormData = new FormData();
+    formData.append('fileKey', fileToUpload, fileToUpload.name);
+    return this.http.post<any>(endpoint, formData);
+}
 
 }
