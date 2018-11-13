a98e43c22e4b9aa15a649582667b558cc9d7fa65
Sebastian Schmidt <schmidtseb@outlook.de>
2018-11-13 22:03:02 +0100

    Added method to VersionLocationData for updating version.

--- backend/src/utils/updateVersions.js
+++ backend/src/utils/updateVersions.js
@@ -1,27 +1,23 @@
+import VersionLocationData from "../models/version-location-data";
+import { increaseVersion } from "../helper/helper-functions";
 
-import VersionLocationData from '../models/version-location-data'
-import {increaseVersion} from '../helper/helper-functions';
-
-module.exports = (poi, res) =>
-poi.type &&
-VersionLocationData.find({type: poi.type}, (err, result) => {
-  if (err) {
-    return next(err)
-  }
-  if (result.length === 0) {
-    const versionLocationData = new VersionLocationData({
-      type: poi.type,
-      version: `v${poi.type.substr(0, 1)}.1.1`
-    })
-    versionLocationData.save(err => null)
-  } else {
-    VersionLocationData.findOneAndUpdate({type: poi.type}, {
-      type: poi.type,
-      version: increaseVersion(result[0].version)
-    }, (err, versionLocationData) => {
-      if (err) {
-        return null
-      }
-    })
-  }
-})
+module.exports = async (poi, res) =>
+  poi.type &&
+  VersionLocationData.find({ type: poi.type }, async (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    if (result.length === 0) {
+      const versionLocationData = new VersionLocationData({
+        type: poi.type,
+        version: `v${poi.type.substr(0, 1)}.1.1`
+      });
+      try {
+        await versionLocationData.save();
+      } catch (err) {}
+    } else {
+      try {
+        await versionLocationData.increaseVersion();
+      } catch (err) {}
+    }
+  });
