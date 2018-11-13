e849e134487dbb640777a232938f2541e8dae0a1
Sebastian Schmidt <schmidtseb@outlook.de>
2018-11-13 23:17:08 +0100

    Fixed updateVersions.

--- backend/src/utils/updateVersions.js
+++ backend/src/utils/updateVersions.js
@@ -3,11 +3,11 @@ import { increaseVersion } from "../helper/helper-functions";
 
 module.exports = async (poi, res) =>
   poi.type &&
-  VersionLocationData.find({ type: poi.type }, async (err, result) => {
+  VersionLocationData.findOne({ type: poi.type }, async (err, vLocData) => {
     if (err) {
       return next(err);
     }
-    if (result.length === 0) {
+    if (!vLocData) {
       const versionLocationData = new VersionLocationData({
         type: poi.type,
         version: `v${poi.type.substr(0, 1)}.1.1`
@@ -17,7 +17,7 @@ module.exports = async (poi, res) =>
       } catch (err) {}
     } else {
       try {
-        await versionLocationData.increaseVersion();
+        await vLocData.increaseVersion();
       } catch (err) {}
     }
   });
