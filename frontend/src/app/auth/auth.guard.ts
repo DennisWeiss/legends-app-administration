2f028ae5012da714405156fc2584b58aea29ebdd
Sebastian Schmidt <schmidtseb@outlook.de>
2018-11-26 21:17:53 +0100

    Fixed typescript-error in route-guard that was caused by importing the wrong Promise.

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -2,9 +2,6 @@ import {Injectable} from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
-import {promise} from "selenium-webdriver";
-import Promise = promise.Promise;
-import {of} from "rxjs/internal/observable/of";
 import {map} from "rxjs/operators";
 
 @Injectable({
@@ -18,7 +15,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): boolean | Observable<boolean> | Promise<boolean> {
+  ): boolean | Promise<boolean> | Observable<boolean>  {
 
     if (this.authService.authState) {
       return true
