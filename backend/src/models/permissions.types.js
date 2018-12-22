40426d8f92e09edf421f9d0456153ee5ef5a7d99
Sebastian Schmidt <schmidtseb@outlook.de>
2018-12-22 22:39:18 +0100

    Implemented function for getting childPermissions, wrote some tests.

--- backend/src/models/permissions.types.js
+++ backend/src/models/permissions.types.js
@@ -1,27 +1,58 @@
-const permissions = {
+/**
+ * PERMISSIONS holds all permissions as key-value-pairs
+ * key: name of the perm as a string
+ * value: object containing children prop that holds an array of children for given permission 
+ * 
+ *  Example: ['EDIT_ALL', {children: ['EDIT_SUBPART', 'READ_ONLY']}]
+ */
 
-    'ADMIN': {children: '*'},
-    'EDIT': {children: ['EDIT_CONTENT']},
-    'EDIT_CONTENT': {children: []},
-    
-    hasChildPerm: function (parent, perm) {
 
-        const childArr = permissions[parent].children;
+export const PERMISSIONS = new Map([
+    ['ADMIN', {children: ['*']}],
+    ['EDIT', {children: ['EDIT_CONTENT']}],
+    ['EDIT_CONTENT', {children: []}]
+  ]
+  );
+  
 
-        if (childArr.length === 0) {return false;}
-      
-        for (const child of childArr) {
-          if (child === perm) { return true; }
-        }
-      
-        return childArr.some(ch => permissions.hasChildPerm(ch, perm));
-    },
 
-    getChildPerms: function() {
+  /**
+   * get all children of permission in a recursive manner
+   * @param {*} parent name of the permission to get all childPerms from
+   */
+  
+  export const getChildPerms = function(parent) {
+    return getChildPerms(PERMISSIONS, parent);
+  }
 
+
+// used for unit testing specifically
+  export const _getChildPerms = function(perms, parent) {
+
+    if(!perms.has(parent)) {
+      throw new Error(`Permission '${parent}' is not defined!`);
+    }
+  
+    const childArr = perms.get(parent).children;
+
+    if(typeof childArr === 'undefined') {
+      throw new Error(`Permission '${parent}' needs to have object with children-prop as value!`)
     }
 
+    if(childArr.length === 0) {return []};
 
-}
+    const result = [];
 
-module.exports = permissions;
+    for(let child of childArr) {
+      result.push(..._getChildPerms(perms, child), child);
+    }
+
+    return result;
+
+  }
+
+
+
+
+
+
