06e1cee3536e5f4ef82d23fe87fb1b5bf4459e52
Sebastian Schmidt <schmidtseb@outlook.de>
2018-12-23 01:08:50 +0100

    Make user-routes only accessible by admins.

--- backend/src/startup/routes.js
+++ backend/src/startup/routes.js
@@ -1,4 +1,8 @@
 module.exports = function(app) {
+
+  const auth = require('../middlewares/authentication')  
+  const permission = require('../middlewares/authorization');
+
   const authRoutes = require("../routes/auth");
   const poiRoutes = require("../routes/poi");
   const contentRoutes = require("../routes/content");
@@ -7,7 +11,7 @@ module.exports = function(app) {
   app.use("/auth", authRoutes);
   app.use("/poi/content", contentRoutes);
   app.use("/poi", poiRoutes);
-  app.use("/users", userRoutes);
+  app.use("/users", auth, permission('ADMIN'), userRoutes);
 
   const errorMiddleware = require('../middlewares/error');
   app.use(errorMiddleware);
